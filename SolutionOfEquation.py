ba7ba1a87ce6f1bc4bad6fca7c285abd6d4c1f50
yuuki08noah <yuuki08noah@gmail.com>
2024-10-08 11:39:59 +0900

    Newton's Method and Bisection Method

---
+++ SolutionOfEquation.py
@@ -0,0 +1,22 @@
+import math
+from decimal import Decimal
+
+class SolutionOfEquation():
+    def __init__(self, equation):
+        self.function = equation
+
+    def bisection_method(self):
+        start, end = Decimal(-10 ** 10), Decimal(10 ** 10)  # 시작 범위
+        for _ in range(1000):
+            mid = (start + end) / Decimal(2.0)
+            if self.function(mid) < 0:
+                start = mid
+            else:
+                end = mid
+        return mid
+
+    def newtons_method(self, init, derivative):
+        x = Decimal(init)
+        for _ in range(1000):
+            x = x - self.function(x) / derivative(x)
+        return x
